067705e7b380580de5d20376544197c9b99f1fca
Mustafa YILDIZ <ayris.dev@gmail.com>
2023-06-22 14:22:23 +0300

    cors

--- configs/nextjs/headers.js
+++ configs/nextjs/headers.js
@@ -1,32 +1,43 @@
 async function headers() {
   return [
     {
-      source: '/:path*',
+      source: "/:path*",
       headers: [
         // security headers from here - https://nextjs.org/docs/advanced-features/security-headers
         {
-          key: 'X-Frame-Options',
-          value: 'SAMEORIGIN',
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN",
         },
         {
-          key: 'X-Content-Type-Options',
-          value: 'nosniff',
+          key: "X-Content-Type-Options",
+          value: "nosniff",
         },
         {
-          key: 'X-XSS-Protection',
-          value: '1; mode=block',
+          key: "X-XSS-Protection",
+          value: "1; mode=block",
         },
         {
-          key: 'X-DNS-Prefetch-Control',
-          value: 'on',
+          key: "X-DNS-Prefetch-Control",
+          value: "on",
         },
         {
-          key: 'Cross-Origin-Opener-Policy',
-          value: 'same-origin',
+          key: "Cross-Origin-Opener-Policy",
+          value: "same-origin",
         },
         {
-          key: 'Referrer-Policy',
-          value: 'origin-when-cross-origin',
+          key: "Referrer-Policy",
+          value: "origin-when-cross-origin",
+        },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value:
+            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
         },
       ],
     },
